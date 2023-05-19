c919dad75a38c4eacc1f0373798225880e633d12
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-19 15:33:06 +0330

    Create footer basic design for website

---
+++ src/components/FooterPages/index.tsx
@@ -0,0 +1,43 @@
+// react hooks
+import { FC } from "react";
+
+// next packages
+import Link from "next/link";
+import Image from "next/image";
+
+export const FooterPages: FC = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900">
+      <div className="w-full max-w-screen-xl mx-auto p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link href="/" className="flex items-center">
+              <Image
+                src="https://flowbite.com/docs/images/logo.svg"
+                alt="FlowBite Logo"
+                width={60}
+                height={60}
+                className="mr-3"
+              />
+              <span className="text-2xl font-semibold self-center whitespace-nowrap dark:text-white">
+                website title
+              </span>
+            </Link>
+          </div>
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-gray-500 text-sm sm:text-center dark:text-gray-400">
+              © 2023{" "}
+              <Link
+                href="https://github.com/arshyajafari"
+                className="hover:underline"
+              >
+                Arshya Jafari
+              </Link>
+              . All Rights Reserved.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
