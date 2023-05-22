52b8cb7ce4d5f907c946c0d9a14609ae65507508
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-22 17:23:03 +0330

    Fixed import files and add comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// next packages
+import type { AppProps } from "next/app";
+
+// import globals.css file
+import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
