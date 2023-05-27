c972af9a96e7cdf405a865372c0d42d18d106154
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-27 21:26:07 +0330

    Add decrease cart item method handler in reducer

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -17,9 +17,24 @@ const shoppingCartSlice = createSlice({
         });
       state.push({ ...action.payload, amount: 1 });
     },
+    decreaseCartItems: (state, action: PayloadAction<number | string>) => {
+      const itemIndex = state.findIndex(
+        (item: any) => item.id === action.payload
+      );
+      if (state[itemIndex].amount === 1) {
+        return state.filter((item: any) => item.id !== action.payload);
+      } else {
+        return state.map((item: any) => {
+          if (item.id === action.payload)
+            return { ...item, amount: item.amount - 1 };
+          else return item;
+        });
+      }
+    },
   },
 });
 
-export const { increaseCartItems } = shoppingCartSlice.actions;
+export const { increaseCartItems, decreaseCartItems } =
+  shoppingCartSlice.actions;
 
 export default shoppingCartSlice.reducer;
