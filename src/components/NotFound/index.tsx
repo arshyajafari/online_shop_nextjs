f12e21d89845578784ff6faa98b26cecafbd959c
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-29 21:56:00 +0330

    Create not found component and import to cart page

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,26 @@
+// react hooks
+import { FC } from "react";
+
+export const NotFound: FC = () => {
+  return (
+    <div className="w-full h-80 flex items-center justify-center my-20">
+      <div className="bg-sky-600 text-white text-2xl font-semibold flex items-center justify-center rounded-xl py-5 px-8">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 22 22"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+          />
+        </svg>
+        <p className="ml-3">Not Found</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -12,6 +12,7 @@ import { Navigation } from "components/Navigation";
 import { ShoppingCart } from "components/ShoppingCart";
 import { FooterPages } from "components/FooterPages";
 import { useSelector } from "react-redux";
+import { NotFound } from "components/NotFound";
 
 // get static data
 const staticData = {
@@ -67,79 +68,79 @@ const ShoppingCartPage: FC = () => {
 
       <Navigation />
 
-      <div className="h-screen my-12">
+      <div className="w-full">
         {products.map((item: any) => (
           <div key={item.id}>
             <ShoppingCart items={item} />
           </div>
         ))}
-      </div>
 
-      {products.length > 0 ? (
-        <Fragment>
-          <div className="max-w-xl my-10 mx-auto">
-            <div className="rounded-lg p-6">
-              <div className="flow-root">
-                <dl className="text-sm leading-5 -my-4 mx-0">
-                  <div className="flex items-center justify-between py-4">
-                    <dt className="text-gray-900 text-lg font-medium leading-6">
-                      Order total
-                    </dt>
-                    <dd className="text-gray-900 text-base font-medium m-0">
-                      ${" "}
-                      {checkedCode === "true"
-                        ? totalPriceWithDiscountCode().toFixed(2)
-                        : totalPrice.toFixed(2)}
-                    </dd>
-                  </div>
-                </dl>
+        {products.length > 0 ? (
+          <Fragment>
+            <div className="max-w-xl my-10 mx-auto">
+              <div className="rounded-lg p-6">
+                <div className="flow-root">
+                  <dl className="text-sm leading-5 -my-4 mx-0">
+                    <div className="flex items-center justify-between py-4">
+                      <dt className="text-gray-900 text-lg font-medium leading-6">
+                        Order total
+                      </dt>
+                      <dd className="text-gray-900 text-base font-medium m-0">
+                        ${" "}
+                        {checkedCode === "true"
+                          ? totalPriceWithDiscountCode().toFixed(2)
+                          : totalPrice.toFixed(2)}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="w-full md:w-1/3 sm:w-1/2 my-4 mx-auto px-6">
-            <form method="post" onSubmit={(e) => discountCodeHandler(e)}>
-              <input
-                type="text"
-                id={checkedCode === "true" ? "success" : "error"}
-                placeholder="Discount code"
-                ref={discountData}
-                className={
-                  checkedCode === ""
-                    ? "w-full text-gray-900 text-sm block border border-gray-500 rounded-lg p-2.5 outline-none placeholder-gray-700"
-                    : checkedCode === "true"
-                    ? "w-full text-green-900 text-sm block border border-green-500 rounded-lg p-2.5 outline-none placeholder-green-700 focus:border-green-500 focus:ring-green-500"
-                    : "w-full text-red-900 text-sm block border border-red-500 rounded-lg p-2.5 outline-none placeholder-red-700 focus:border-red-500 focus:ring-red-500"
-                }
-              />
-              <p
-                className={
-                  checkedCode === ""
-                    ? "text-gray-600 text-sm mt-2"
-                    : checkedCode === "true"
-                    ? "text-green-600 text-sm mt-2"
-                    : "text-red-600 text-sm mt-2"
-                }
-              >
-                {checkedCode === "" ? (
-                  ""
-                ) : checkedCode === "true" ? (
-                  <span className="font-medium">Well done!</span>
-                ) : (
-                  <span className="font-medium">
-                    Unfortunately, the discount code is wrong!
-                  </span>
-                )}
-              </p>
-              <button className="w-full bg-sky-600 text-white lg:text-sm sm:text-xs font-medium uppercase leading-normal rounded-md my-5 px-6 md:px-2 py-2.5 hover:bg-sky-700">
-                Check code
-              </button>
-            </form>
-          </div>
-        </Fragment>
-      ) : (
-        ""
-      )}
+            <div className="w-full md:w-1/3 sm:w-1/2 my-4 mx-auto px-6">
+              <form method="post" onSubmit={(e) => discountCodeHandler(e)}>
+                <input
+                  type="text"
+                  id={checkedCode === "true" ? "success" : "error"}
+                  placeholder="Discount code"
+                  ref={discountData}
+                  className={
+                    checkedCode === ""
+                      ? "w-full text-gray-900 text-sm block border border-gray-500 rounded-lg p-2.5 outline-none placeholder-gray-700"
+                      : checkedCode === "true"
+                      ? "w-full text-green-900 text-sm block border border-green-500 rounded-lg p-2.5 outline-none placeholder-green-700 focus:border-green-500 focus:ring-green-500"
+                      : "w-full text-red-900 text-sm block border border-red-500 rounded-lg p-2.5 outline-none placeholder-red-700 focus:border-red-500 focus:ring-red-500"
+                  }
+                />
+                <p
+                  className={
+                    checkedCode === ""
+                      ? "text-gray-600 text-sm mt-2"
+                      : checkedCode === "true"
+                      ? "text-green-600 text-sm mt-2"
+                      : "text-red-600 text-sm mt-2"
+                  }
+                >
+                  {checkedCode === "" ? (
+                    ""
+                  ) : checkedCode === "true" ? (
+                    <span className="font-medium">Well done!</span>
+                  ) : (
+                    <span className="font-medium">
+                      Unfortunately, the discount code is wrong!
+                    </span>
+                  )}
+                </p>
+                <button className="w-full bg-sky-600 text-white lg:text-sm sm:text-xs font-medium uppercase leading-normal rounded-md my-5 px-6 md:px-2 py-2.5 hover:bg-sky-700">
+                  Check code
+                </button>
+              </form>
+            </div>
+          </Fragment>
+        ) : (
+          <NotFound />
+        )}
+      </div>
 
       <FooterPages />
     </Fragment>
