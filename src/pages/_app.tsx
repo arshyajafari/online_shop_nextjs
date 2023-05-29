4f66ce95efa5ac814eeb932b11c644b7089bc23b
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-29 16:36:15 +0330

    Replace provider from home page to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 // next packages
 import type { AppProps } from "next/app";
 
+// import redux package
+import { Provider } from "react-redux";
+
+// import store file
+import { shoppingCartStore } from "store";
+
 // import globals.css file
 import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={shoppingCartStore}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
-// import redux package
-import { Provider } from "react-redux";
-
-// import store file
-import { shoppingCartStore } from "store";
-
 // import main file
 import ProductsPage from "./products";
 
 export default function Home() {
-  return (
-    <Provider store={shoppingCartStore}>
-      <ProductsPage />
-    </Provider>
-  );
+  return <ProductsPage />;
 }
