2dcbf5928358428ac9105cc6cd247b9c7f257cc8
Arshya Jafari <arshyajafari11@gmail.com>
2023-05-31 20:39:54 +0330

    Update _app.tsx: import meta, favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 
 // next packages
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 // import redux package
 import { Provider } from "react-redux";
@@ -16,9 +17,24 @@ import { ToastContainer } from "react-toastify";
 // import globals.css file
 import "styles/globals.css";
 
+// import image
+import logo from "assets/img/logo.svg";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Web site created using next-js and typescript"
+        />
+
+        <link rel="icon" href={logo.src} />
+
+        <title>Online Shop</title>
+      </Head>
+
       <Provider store={shoppingCartStore}>
         <Component {...pageProps} />
       </Provider>
